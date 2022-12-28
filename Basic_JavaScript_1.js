b040c2299c937cd1c85bec4c626f694a78b6e577
LoafyLilu <liluloaf@gmail.com>
2022-12-28 14:22:14 -0800

    Moving files
    Moving files to appropriate repository

---
+++ Basic_JavaScript_1.js
@@ -0,0 +1,8 @@
+<script>
+	function My_First_Function() {
+		var String ="Kiss me, I'm Irish!";
+		var result = String.fontcolor("green");
+		document.getElemenById("Irish").innerHTML =
+		result;
+	}
+</script>
---
+++ README (2).md
@@ -0,0 +1,2 @@
+# Basic_HTML_and_Javascript_1
+ This repository is for HTML and Javascript based projects
