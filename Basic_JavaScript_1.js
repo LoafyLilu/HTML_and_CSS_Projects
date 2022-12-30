087c238755f91500e24fa53000bdfb73f62db699
LoafyLilu <liluloaf@gmail.com>
2022-12-30 09:39:25 -0800

    .

--- Basic_JavaScript_1.js
+++
@@ -1,8 +0,0 @@
-<script>
-	function My_First_Function() {
-		var String ="Kiss me, I'm Irish!";
-		var result = String.fontcolor("green");
-		document.getElemenById("Irish").innerHTML =
-		result;
-	}
-</script>
